b257b34f679ba4f190195fc9d26f7e2224cda281
luiggi peotta <luiggi.peotta@gmail.com>
2025-03-13 18:46:03 -0300

    Feat Class Make Number and Make Data

--- telecom.py
+++ telecom.py
@@ -1,2 +1,11 @@
 from dados import MakeNumber
+from dados import MakeData
 
+md = MakeData()
+mn = MakeNumber()
+print(mn.numberRandom())
+print(md.debtorRandom())
+print(md.dataStart())
+print(md.durationRandom())
+print(md.entry_Route())
+print(md.exit_Route())
