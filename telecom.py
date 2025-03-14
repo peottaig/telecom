2f7cbb93f65a23afe8b151433e8f7648390bc9ad
luiggi peotta <luiggi.peotta@gmail.com>
2025-03-14 08:59:57 -0300

    feat: init telecom

--- telecom.py
+++ telecom.py
@@ -3,3 +3,28 @@ from dados import MakeData
 
 md = MakeData()
 mn = MakeNumber()
+
+class files:
+    
+    def __init__(self):
+        super().__init__()
+        self.md = MakeData()
+        self.mn = MakeNumber()
+        self.numberDestiny = mn.numberRandom()
+        self.numberOrigin = mn.numberRandom()
+        self.debtor = md.debtorRandom()
+        self.dataStart = md.dataStart()
+        self.duration = md.durationRandom()
+        self.entry_Route = md.entry_Route()
+        self.exit_Route = md.exit_Route()
+        self.dataDict = {
+            self.numberDestiny: 16,
+            self.numberOrigin: 25,
+            self.debtor: 1,
+            self.dataStart: 14,
+            self.duration: 6,
+            self.entry_Route: 4,
+            self.exit_Route: 4
+        }
+
+    
